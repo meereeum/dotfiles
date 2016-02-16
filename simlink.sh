bc7a8657e0c3588d98dd90f8d8fabe75d12da4c0
Miriam Shiffman <meereeum@gmail.com>
2016-02-16 16:01:21 -0500

    easy restore apt-get installed packages

--- simlink.sh
+++ simlink.sh
@@ -4,3 +4,9 @@ for f in bash_profile gitconfig spacemacs vimrc screenrc
 	do rm ~/.${f}
 	ln -s ~/dotfiles/.${f} ~/.${f}
 done
+
+
+# also install apt-get packages if on linux os
+if (($linux)); then
+	cat packages.txt | xargs sudo apt-get install
+fi
