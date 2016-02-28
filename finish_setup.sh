a7911310d04e2342f917ef8bbd50a4b2d62891b3
Miriam Shiffman <meereeum@gmail.com>
2016-02-28 17:24:22 -0500

    finish setup after following initial instructions

---
+++ finish_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+if (($linux)); then
+
+	# install apt-get packages
+	while read line
+		#do sudo apt-get install -y ${line}
+		do sudo apt-get install ${line}
+	done < packages.txt
+
+	# update uq vpn profile
+	cp template_uq.conf uq.conf
+	vi uq.conf
+	sudo mv uq.conf /etc/vpnc
+fi
