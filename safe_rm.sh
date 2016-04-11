37fc12caa07b599eda7e78bacb913137d3187d00
Miriam Shiffman <meereeum@gmail.com>
2016-04-11 16:15:07 -0400

    set path/to/Trash depending on OS

--- safe_rm.sh
+++ safe_rm.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
+if (($linux)); then
+    TRASHDIR="/home/miriam/.local/share/Trash/files"
+else
+    TRASHDIR="~/.Trash"
+fi
+
+
 for file in "$@"; do
     if [ -e "${file}" ]; then
 
         # Target exists and can be moved to Trash safely
-        if [ ! -e ~/.Trash/"${file}" ]; then
-            mv "${file}" ~/.Trash/
+        if [ ! -e "${TRASHDIR}/${file}" ]; then
+            mv "${file}" ${TRASHDIR}
 
             # Target exists and conflicts with target in Trash
-        elif [ -e ~/.Trash/"${file}" ]; then
+        elif [ -e "${TRASHDIR}/${file}" ]; then
 
             # Increment target name until
             # there is no longer a conflict
             i=1
-            while [ -e ~/.Trash/"${file}".$i ];
+            while [ -e "${TRASHDIR}/${file}.${i}" ];
             do
                 i=$(($i + 1))
             done
 
             # Move to the Trash with non-conflicting name
-            mv "${file}" ~/.Trash/"${file}".$i
+            mv "${file}" "${TRASHDIR}/${file}.{$i}"
         fi
 
         # Target doesn't exist, return error
