fea3564196ba36dca6ecc932ae0ebd46e9a5e41a
Miriam Shiffman <meereeum@gmail.com>
2018-01-30 14:27:49 -0500

    script autoupdating pkg lists

---
+++ update_pkgs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PKGDIR="$( dirname "$0" )/packages"
+
+# conda
+
+conda env export -n root > "${PKGDIR}/conda_py27.yml" &&
+conda env export -n py36 > "${PKGDIR}/conda_py36.yml" &&
+
+echo "updated: ${PKGDIR}/conda_py{27,36}.yml"
+
+if (($linux)); then
+	DISTRO=$( cat /etc/*-release | awk -F'=' '/^NAME/ {print $2}' | xargs )
+	
+	if [[ $DISTRO = "Ubuntu" ]]; then # server
+		OUTFILE="packages_server.txt"
+	else
+		OUTFILE="packages.txt"
+	fi
+
+	# apt
+	pkgs > "${PKGDIR}/${OUTFILE}"
+else
+	# homebrew
+	OUTFILE="packages_brew.txt"
+	brew list > "${PKGDIR}/${OUTFILE}"
+fi &&
+
+echo "updated: ${PKGDIR}/${OUTFILE}"
