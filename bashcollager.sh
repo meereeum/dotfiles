c24c07ca3ad796ca75518779d690d7379c413d54
Miriam Shiffman <meereeum@gmail.com>
2018-08-08 00:02:05 -0400

    bettercollager

--- bashcollager.sh
+++ bashcollager.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
 
+
 F_TIMES="${HOME}/collagertimes"
+
 
 vlc_hook_off(){
     f=$( basename "$@" )
-    TMP="${F_TIMES}.tmp"
 
-    # remove preexisting
-    grep -v "$f" $F_TIMES > $TMP
+    VALIDTIME="^([0-9]+:?)+$"
+    REMOVE="q"
 
-    read -p "Enter stoptime (or q): " stoptime
+    read -p "Enter stoptime (or $REMOVE): " stoptime
 
-    # update (or leave removed)
-    [[ ${stoptime,,} == q ]] || echo -e "$f\t$stoptime" >> $TMP
+    # remove preexisting (if valid time or "$REMOVE"/ENTER)
+    [[ ${stoptime,,} =~ $VALIDTIME|^$REMOVE?$ ]] && sed -i "/$f/d" $F_TIMES
 
-    \mv $TMP $F_TIMES
+    # update if valid (or leave removed)
+    [[ ${stoptime} =~ $VALIDTIME ]] && echo -e "$f\t$stoptime" >> $F_TIMES
 }
+
 
 vlc_hook_on(){
     f=$( basename "$@" )
-    starttime=""
 
     # pattern match filename & get time
     starttime=$( awk -F'\t' -v f="$f" '$1==f{print $2}' $F_TIMES )
     [[ $starttime ]] || starttime=0 # default to beginning
 
     # total seconds
-    startsecs=$( echo $starttime | awk -F':' '{out=""; for (i=1; i<=NF; i++) out=out+$i * (60^(NF-i)) } END {print out}' )
+    startsecs=$( echo $starttime | awk -F':' '{out=0; for (i=1; i<=NF; i++) out=out + $i * 60^(NF-i) } END {print out}' )
 
     vlc --start-time $startsecs "$@"
 }
-
-#restart(){
-#    touch $F_TIMES
-#
-#    for f in "$@"; do
-#        vlc_hook_on "$f" && vlc_hook_off "$f"
-#    done
-#}
 
 
 touch $F_TIMES
