f430c034cf44447c0c58e2eb19ab082ea1e49455
Miriam Shiffman <meereeum@gmail.com>
2018-09-04 13:46:02 -0400

    add remote prompt with hostname

--- cluster_setup.sh
+++ cluster_setup.sh
@@ -53,5 +53,11 @@ wget $CONDA -O ~/conda.sh && \
 sed -ri'.tmp' --follow-symlinks 's/^(.*pandoc)/#\1/' ~/.bash_profile
 sed -ri'.tmp' --follow-symlinks 's/^(.*safe_rm)/#\1/' ~/.bash_profile
 
+
+# edit for remote prompt
+
+sed -ri'.tmp' --follow-symlinks 's/^(export PS1=).*$/\1"\e[1m\h:\e[21m \W \$ "/' ~/.bash_profile
+
+
 # does not exist or remove
 [ ! -e ~/.bash_profile.tmp ] || rm ~/.bash_profile.tmp
