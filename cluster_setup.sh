ff017afdf5989fb027cd94e8c8e7eb8127d80566
Miriam Shiffman <meereeum@gmail.com>
2018-09-05 23:44:11 -0400

    proper slashes

--- cluster_setup.sh
+++ cluster_setup.sh
@@ -56,7 +56,7 @@ sed -ri'.tmp' --follow-symlinks 's/^(.*safe_rm)/#\1/' ~/.bash_profile
 
 # edit for remote prompt
 
-sed -ri'.tmp' --follow-symlinks 's/^(export PS1=).*$/\1"\e[1m\h:\e[21m \W \$ "/' ~/.bash_profile
+sed -ri'.tmp' --follow-symlinks 's/^(export PS1=).*$/\\1"\\e[1m\\h:\\e[21m \\W \\$ "/' ~/.bash_profile
 
 
 # does not exist or remove
