7c2197178ad1ee0c3733bddda17b2f43fdbaa9b5
meereeum <meereeum@gmail.com>
2020-02-11 17:27:08 -0500

    Δs from broad

--- update_pkgs.sh
+++ update_pkgs.sh
@@ -1,37 +1,36 @@
 #!/usr/bin/env bash
 
-PKGDIR="$( dirname "$0" )/packages"
+PKGDIR="$( dirname "$0" )/pkgs"
 
-# conda
+[[ $DISPLAY ]] && SUFFIX="" || SUFFIX="_server"
 
-#conda env export -n root > "${PKGDIR}/conda_py27.yml" &&
-#conda env export -n py36 > "${PKGDIR}/conda_py36.yml" &&
-VPY=$( python --version 2>&1 | sed -E 's/^Python ([0-9])\.([0-9]).*$/\1\2/' ) # NB python --version goes to STDERR, not -OUT
-conda env export -n root > "${PKGDIR}/conda_py${VPY}.yml" &&
 
-echo "updated: ${PKGDIR}/conda_py${VPY}.yml"
-#echo "updated: ${PKGDIR}/conda_py{27,36}.yml"
+# firefox addons
+[[ $DISPLAY ]] && getAddons > "$PKGDIR/ffox_addons.txt" &&
+    echo "updated: $PKGDIR/ffox_addons.txt"
 
+
+# python pkgs
+VPY=$( python --version 2>&1 | sed -E 's/^Python ([0-9])\.([0-9]).*$/\1\2/' ) # N.B. python --version goes to STDERR, not -OUT
+conda env export -n root > "$PKGDIR/conda_py$VPY$SUFFIX.yml" &&
+    echo "updated: $PKGDIR/conda_py$VPY$SUFFIX.yml"
+
+
+# system pkgs
 if (($linux)); then
 	DISTRO=$( cat /etc/*-release | awk -F'=' '/^NAME/ {print $2}' | xargs )
-	
-	if [[ $DISTRO = "Ubuntu" ]]; then # server
-		OUTFILE="packages_server.txt"
-	else
-		OUTFILE="packages.txt"
-	fi
 
 	# apt
-    source ~/.bash_profile
-	pkgs > "${PKGDIR}/${OUTFILE}"
+    OUTFILE="aptpkgs$SUFFIX.txt"
+	pkgs > "$PKGDIR/$OUTFILE"
 else
 	# homebrew
-	OUTFILE="packages_brew.txt"
-	brew list > "${PKGDIR}/${OUTFILE}"
-	echo "updated: ${PKGDIR}/${OUTFILE}"
+	OUTFILE="brewpkgs.txt"
+	brew list > "$PKGDIR/$OUTFILE"
+	echo "updated: $PKGDIR/$OUTFILE"
 
-	OUTFILE="packages_brew_cask.txt"
-	brew cask list > "${PKGDIR}/${OUTFILE}"
+	OUTFILE="brewpkgs_cask.txt"
+	brew cask list > "$PKGDIR/$OUTFILE"
 fi &&
 
-echo "updated: ${PKGDIR}/${OUTFILE}"
+echo "updated: $PKGDIR/$OUTFILE"
