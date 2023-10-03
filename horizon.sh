b457ab67250c88512056c31c4c43816928d71b05
meereeum <meereeum@gmail.com>
2023-10-03 14:53:34 +0000

    fixing darksky

--- horizon.sh
+++ horizon.sh
@@ -4,7 +4,7 @@ DIR=$( dirname "${BASH_SOURCE[0]}" )
 [[ -f $DIR/SECRET_darksky ]] && KEY=$( cat $DIR/SECRET_darksky ) \
                              || ( >&2 echo "missing SECRET_darksky" && exit 1 ) # echo to STDERR & leave
 
-EXCLUDE=currently,minutely,hourly,alerts,flags
+# EXCLUDE=currently,minutely,hourly,alerts,flags
 
 # LATLON=42.3566978,-71.1072584 # @ 589
 # LATLON=40.6695668,-73.936074  # @ 961
@@ -14,10 +14,11 @@ LATLON=$( cat /tmp/latlon )
 # need to rerun ?
 [[ -f /tmp/darksky ]] && {
     SUNTIMES=$( cat /tmp/darksky |
-                jq -c '.daily.data[0] | [.sunriseTime, .sunsetTime]' )
-    
-    SUNRISE=$( echo $SUNTIMES | jq '.[0]' )
-    SUNSET=$(  echo $SUNTIMES | jq '.[1]' )
+                # jq -c '.daily.data[0] | [.sunriseTime, .sunsetTime]' )
+                jq -c '.location.values[0] | [.sunrise,.sunset]' )
+
+    SUNRISE=$( echo $SUNTIMES | jq '.[0]' | xargs date +%s -d )
+    SUNSET=$(  echo $SUNTIMES | jq '.[1]' | xargs date +%s -d )
     NOW=$( date +%s )
 
     if [[ ! $SUNTIMES ]] || [[ $SUNTIMES == "[null,null]" ]]; then
@@ -29,5 +30,6 @@ LATLON=$( cat /tmp/latlon )
     fi
 } || RERUN=1
 
-(( $RERUN )) && curl -s https://api.darksky.net/forecast/$KEY/$LATLON?exclude=$EXCLUDE \
+# (( $RERUN )) && curl -s https://api.darksky.net/forecast/$KEY/$LATLON?exclude=$EXCLUDE \
+(( $RERUN )) && curl -s "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?aggregateHours=24&includeAstronomy=true&forecastDays=1&contentType=json&combinationMethod=aggregate&unitGroup=us&locationMode=single&key=$KEY&locations=$LATLON" \
                     > /tmp/darksky
